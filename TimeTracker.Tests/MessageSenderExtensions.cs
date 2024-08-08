8e5853c94a2728952ab342b3a75019cfa975a7a3
Artemious <16724889+artemious7@users.noreply.github.com>
2024-08-08 18:57:11 +0100

    add null checks in MessageSenderExtensions

--- TimeTracker.Tests/MessageSenderExtensions.cs
+++ TimeTracker.Tests/MessageSenderExtensions.cs
@@ -8,11 +8,15 @@ public static class MessageSenderExtensions
 {
     public static async Task DidNotSendAnything(this MessageSender messageSender)
     {
+        ArgumentNullException.ThrowIfNull(messageSender);
+
         await messageSender.DidNotReceiveWithAnyArgs().Invoke(Arg.Any<string>());
     }
 
     public static async Task SentOnly(this MessageSender messageSender, string message)
     {
+        ArgumentNullException.ThrowIfNull(messageSender);
+
         await messageSender.Received(1).Invoke(message);
         messageSender.ReceivedCalls().Should().ContainSingle();
     }
