5b710e5795321bc856aa464cd71e182d776f521d
Artemious <16724889+artemious7@users.noreply.github.com>
2024-08-08 13:13:06 +0100

    red GivenHelpCommand_WhenTryHandle_ThenSendsHelpMessageAndReturnsTrue

--- TimeTracker.Tests/HelpHandlerFixture.cs
+++ TimeTracker.Tests/HelpHandlerFixture.cs
@@ -6,16 +6,22 @@ namespace TimeTracker.Tests;
 
 public class HelpHandlerFixture
 {
+    private readonly HelpHandler sut;
+
+    public HelpHandlerFixture()
+    {
+        sut = new HelpHandler();
+    }
+
     [Fact]
     public async Task GivenAnyMessage_WhenTryHandle_ThenReturnsFalse()
     {
         // Arrange
         string message = "any message";
         var messageSender = Substitute.For<MessageSender>();
-        var handler = new HelpHandler();
 
         // Act
-        bool handled = await handler.TryHandle(message, messageSender);
+        bool handled = await sut.TryHandle(message, messageSender);
 
         // Assert
         handled.Should().BeFalse();
@@ -23,8 +29,17 @@ public class HelpHandlerFixture
     }
 
     [Fact]
-    public void GivenHelpCommand_WhenTryHandle_ThenSendsHelpMessageAndReturnsTrue()
+    public async Task GivenHelpCommand_WhenTryHandle_ThenSendsHelpMessageAndReturnsTrue()
     {
+        // Arrange
+        string message = "/help";
+        var messageSender = Substitute.For<MessageSender>();
 
+        // Act
+        bool handled = await sut.TryHandle(message, messageSender);
+
+        // Assert
+        handled.Should().BeTrue();
+        await messageSender.Received(1).Invoke($"Send me the time, I will sum it up for you, e.g. `1:35` or `15:45 - 16:20` to add, or `-0:20` to subtract.  \r\nCommands:  \r\n /showTotal  \r\n /undo  \r\n /reset  \r\n /help");
     }
 }
