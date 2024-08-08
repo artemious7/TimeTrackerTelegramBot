9924ff6e14077c9693d85ed6393f04924c2da965
Artemious <16724889+artemious7@users.noreply.github.com>
2024-08-08 13:01:51 +0100

    create TimeTracker shared library; create HelpHandler with a red test GivenAnyMessage_WhenTryHandle_ThenReturnsFalse

---
+++ TimeTracker.Tests/HelpHandlerFixture.cs
@@ -0,0 +1,29 @@
+﻿using FluentAssertions;
+using NSubstitute;
+using TimeTrackerBot.Services;
+
+namespace TimeTracker.Tests;
+
+public class HelpHandlerFixture
+{
+    [Fact]
+    public async Task GivenAnyMessage_WhenTryHandle_ThenReturnsFalse()
+    {
+        // Arrange
+        string message = "any message";
+        var messageSender = Substitute.For<MessageSender>();
+        var handler = new HelpHandler();
+
+        // Act
+        bool handled = await handler.TryHandle(message, messageSender);
+
+        // 
+        handled.Should().BeFalse();
+    }
+
+    [Fact]
+    public void GivenHelpCommand_WhenTryHandle_ThenSendsHelpMessageAndReturnsTrue()
+    {
+
+    }
+}
---
+++ TimeTracker/Handlers/HelpHandler.cs
@@ -0,0 +1,16 @@
+﻿using TimeTrackerBot.Services;
+
+namespace TimeTracker
+{
+    public class HelpHandler
+    {
+        public HelpHandler()
+        {
+        }
+
+        public async Task<bool> TryHandle(string message, MessageSender messageSender)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
