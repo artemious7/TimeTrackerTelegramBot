872bf3127f6c9521fc616602e338189e0c27bbb4
Artemious <16724889+artemious7@users.noreply.github.com>
2024-08-08 19:16:33 +0100

    red GivenEmptyListOfHandlers_WhenProcess_ThenDoNothing

---
+++ TimeTracker.Tests/Services/ResponderFixture.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.Internal;
+using NSubstitute;
+using TimeTrackerBot.Services;
+using TimeTrackerBot.TimeTracker;
+
+namespace TimeTracker.Tests.Services;
+
+public class ResponderFixture
+{
+    private readonly MessageSender messageSender = Substitute.For<MessageSender>();
+
+    [Fact]
+    public async Task GivenEmptyListOfHandlers_WhenProcess_ThenDoNothing()
+    {
+        // Arrange
+        string message = "any message";
+        List<IHandler> handlers = [];
+        var originalData = new UserData(default, default, default);
+        var sut = new Responder(message, originalData, messageSender, handlers);
+
+        // Act
+        var newData = await sut.Process();
+
+        // Assert
+        newData.Should().Be(originalData);
+    }
+}
