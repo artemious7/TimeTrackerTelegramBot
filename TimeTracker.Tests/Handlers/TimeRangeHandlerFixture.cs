61b4d6dca9d76c4d03e8a676d7090d63dfa6c261
Artemious <16724889+artemious7@users.noreply.github.com>
2024-08-09 11:02:21 +0100

    green GivenTimeMessage_WhenTryHandle_ThenReturnsFalse

--- TimeTracker.Tests/Handlers/TimeRangeHandlerFixture.cs
+++ TimeTracker.Tests/Handlers/TimeRangeHandlerFixture.cs
@@ -20,4 +20,22 @@ public class TimeRangeHandlerFixture
         handled.Should().BeFalse();
         await messageSender.DidNotSendAnything();
     }
+
+    [Theory]
+    [InlineData("1:05")]
+    [InlineData("-1:00")]
+    [InlineData("- 0:05")]
+    public async Task GivenTimeMessage_WhenTryHandle_ThenReturnsFalse(string message)
+    {
+        // Arrange
+        var messageSender = Substitute.For<MessageSender>();
+        var data = new UserData(default, default, default);
+
+        // Act
+        bool handled = await sut.TryHandle(message, data, messageSender);
+
+        // Assert
+        handled.Should().BeFalse();
+        await messageSender.DidNotSendAnything();
+    }
 }
