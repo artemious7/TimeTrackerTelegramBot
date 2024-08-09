607332ebf9f63bbb94f98b7b08047c307fc8e367
Artemious <16724889+artemious7@users.noreply.github.com>
2024-08-09 10:57:23 +0100

    TimeRangeHandlerFixture: red GivenAnyOtherMessage_WhenTryHandle_ThenReturnsFalse

---
+++ TimeTracker.Tests/Handlers/TimeRangeHandlerFixture.cs
@@ -0,0 +1,22 @@
+﻿namespace TimeTracker.Tests.Handlers;
+
+public class TimeRangeHandlerFixture
+{
+    private readonly TimeRangeHandler sut = new();
+
+    [Fact]
+    public async Task GivenAnyOtherMessage_WhenTryHandle_ThenReturnsFalse()
+    {
+        // Arrange
+        string message = "any other message";
+        var messageSender = Substitute.For<MessageSender>();
+        var data = new UserData(default, default, default);
+
+        // Act
+        bool handled = await sut.TryHandle(message, data, messageSender);
+
+        // Assert
+        handled.Should().BeFalse();
+        await messageSender.DidNotSendAnything();
+    }
+}
---
+++ TimeTracker/Handlers/TimeRangeHandler.cs
@@ -0,0 +1,15 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using TimeTracker.Handlers;
+using TimeTrackerBot.Services;
+using TimeTrackerBot.TimeTracker;
+
+namespace TimeTracker
+{
+    public class TimeRangeHandler : IHandler
+    {
+        public Task<HandleResult> TryHandle([AllowNull] string message, UserData? data, MessageSender SendMessage)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
