ab97e709125de777593fce566052d84d990a6f80
Artemious <16724889+artemious7@users.noreply.github.com>
2024-08-09 11:09:10 +0100

    red GivenTimeRangeMessage_WhenTryHandle_ThenAddsTimeAndRespondsAndReturnsTrue

--- TimeTracker.Tests/Handlers/TimeRangeHandlerFixture.cs
+++ TimeTracker.Tests/Handlers/TimeRangeHandlerFixture.cs
@@ -4,13 +4,13 @@ namespace TimeTracker.Tests.Handlers;
 public class TimeRangeHandlerFixture
 {
     private readonly TimeRangeHandler sut = new();
+    private readonly MessageSender messageSender = Substitute.For<MessageSender>();
 
     [Fact]
     public async Task GivenAnyOtherMessage_WhenTryHandle_ThenReturnsFalse()
     {
         // Arrange
         string message = "any other message";
-        var messageSender = Substitute.For<MessageSender>();
         var data = new UserData(default, default, default);
 
         // Act
@@ -28,7 +28,6 @@ public class TimeRangeHandlerFixture
     public async Task GivenTimeMessage_WhenTryHandle_ThenReturnsFalse(string message)
     {
         // Arrange
-        var messageSender = Substitute.For<MessageSender>();
         var data = new UserData(default, default, default);
 
         // Act
@@ -37,5 +36,23 @@ public class TimeRangeHandlerFixture
         // Assert
         handled.Should().BeFalse();
         await messageSender.DidNotSendAnything();
+    }
+
+    [Theory]
+    [InlineData("0:00-1:05")]
+    [InlineData("15:00-16:05")]
+    [InlineData("17:30 - 18:35")]
+    public async Task GivenTimeRangeMessage_WhenTryHandle_ThenAddsTimeAndRespondsAndReturnsTrue(string message)
+    {
+        // Arrange
+        var originalData = new UserData(Time: TimeSpan.FromHours(4), Started: default, PreviousTime: null);
+
+        // Act
+        var (handled, newData) = await sut.TryHandle(message, originalData, messageSender);
+
+        // Assert
+        handled.Should().BeTrue();
+        await messageSender.SentOnly($"Added 1:05. Total time recorded: 5:05");
+        newData.Should().Be(new UserData(Time: TimeSpan.FromHours(5) + TimeSpan.FromMinutes(5), Started: default, PreviousTime: originalData.Time));
     }
 }
