922c4b187b1d65c3d59a3da33cb35c8bbc650f6b
Artemious <16724889+artemious7@users.noreply.github.com>
2024-09-10 20:22:53 +0100

    green GivenListOfHandlers_WhenProcessAndAllAHandlerReturnsTrueAndNewData_ThenOnlyHandlersUpToThatOneAreCalledAndReturnsNewData

--- src/TimeTracker.Tests/Services/ResponderFixture.cs
+++ src/TimeTracker.Tests/Services/ResponderFixture.cs
@@ -3,12 +3,12 @@
 public class ResponderFixture
 {
     private readonly MessageSender messageSender = Substitute.For<MessageSender>();
+    private readonly string message = "any message";
 
     [Fact]
     public async Task GivenEmptyListOfHandlers_WhenProcess_ThenDoNothing()
     {
         // Arrange
-        string message = "any message";
         List<IHandler> handlers = [];
         var originalData = new UserData(default, default, default);
         var sut = new Responder(message, originalData, messageSender, handlers);
@@ -24,8 +24,7 @@ public class ResponderFixture
     public async Task GivenListOfHandlers_WhenProcessAndAllHandlersReturnFalse_ThenAllHandlersCalledInOrder()
     {
         // Arrange
-        string message = "any message";
-        List<IHandler> handlersInput = [.. Enumerable.Range(1, 10).Select(r => Substitute.For<IHandler>())];
+        List<IHandler> handlersInput = [.. Enumerable.Range(1, 10).Select(_ => Substitute.For<IHandler>())];
         List<IHandler> calledHandlers = [];
         handlersInput.ForEach(handler => handler
             .WhenForAnyArgs(r => r.TryHandle(default, default, messageSender))
@@ -40,5 +39,33 @@ public class ResponderFixture
         newData.Should().Be(originalData, "user data should not be changed unless changed by handlersInput");
         handlersInput.ForEach(r => _ = r.Received(1).TryHandle(message, originalData, messageSender));
         calledHandlers.Should().Equal(handlersInput);
+    }
+
+    [Fact]
+    public async Task GivenListOfHandlers_WhenProcessAndAllAHandlerReturnsTrueAndNewData_ThenOnlyHandlersUpToThatOneAreCalledAndReturnsNewData()
+    {
+        // Arrange
+        var trueHandler = Substitute.For<IHandler>();
+
+        List<IHandler> handlersInput = [.. Enumerable.Range(1, 4).Select(_ => Substitute.For<IHandler>()), trueHandler, .. Enumerable.Range(1, 4).Select(_ => Substitute.For<IHandler>())];
+        List<IHandler> calledHandlers = [];
+        var originalData = new UserData(TimeSpan.FromHours(1), default, TimeSpan.FromHours(2));
+        var newData = new UserData(TimeSpan.FromHours(2), default, TimeSpan.FromHours(3));
+        trueHandler.TryHandle(default, default, messageSender)
+            .ReturnsForAnyArgs(Task.FromResult(new HandleResult(true, newData)));
+        handlersInput
+            .ForEach(handler => handler
+                .WhenForAnyArgs(r => r.TryHandle(default, default, messageSender))
+                .Do(c => calledHandlers.Add(handler)));
+        var sut = new Responder(message, originalData, messageSender, handlersInput);
+
+        // Act
+        var newDataReturned = await sut.Process();
+
+        // Assert
+        newDataReturned.Should().Be(newData, "user data should be changed by trueHandler");
+        handlersInput.Take(5).ToList().ForEach(r => _ = r.Received(1).TryHandle(message, originalData, messageSender));
+        handlersInput.Skip(5).ToList().ForEach(r => _ = r.DidNotReceiveWithAnyArgs().TryHandle(default, default, messageSender));
+        calledHandlers.Should().Equal(handlersInput.Take(5));
     }
 }
