83796e9d2324a0f6226c70b415f1e0411aa0400e
Artemious <16724889+artemious7@users.noreply.github.com>
2024-10-09 22:01:05 +0100

    add azurerm backend

--- infra/providers.tf
+++ infra/providers.tf
@@ -1,5 +1,6 @@
 terraform {
   required_version = "~> 1.6"
+
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
@@ -10,10 +11,20 @@ terraform {
       version = ">= 3.5.0, < 4.0.0"
     }
   }
+
+  backend "azurerm" {
+    resource_group_name  = "tfstate"
+    storage_account_name = "tfstate85581740"
+    container_name       = "tfstate"
+    key                  = "terraform.tfstate"
+    use_oidc             = true
+  }
 }
 
 # tflint-ignore: terraform_module_provider_declaration, terraform_output_separate, terraform_variable_separate
 provider "azurerm" {
+  use_oidc = true
+  
   features {
     resource_group {
       prevent_deletion_if_contains_resources = false
