bfd04abf79c6106ac05f1c501cfc230bc23fac93
Artemious <16724889+artemious7@users.noreply.github.com>
2024-10-22 22:14:36 +0100

    rename to CD.yml

--- build-github-actions/act-cli.sh
+++ build-github-actions/act-cli.sh
@@ -1,4 +1,4 @@
 docker build -t my-build-server:latest ./build-github-actions
 
 # Act CLI docs: https://nektosact.com/usage/index.html
-clear | act  -W ".github/workflows/provision-deploy.yml"  -s GITHUB_TOKEN="$(gh auth token)"  -P ubuntu-latest=my-build-server:latest  --secret-file my.secrets  --artifact-server-path act-cli-temp-artifacts  --var-file configurationVariables.secrets --pull=false # -j 'provision' 
+clear | act -W ".github/workflows/CD.yml" -s GITHUB_TOKEN="$(gh auth token)" -P ubuntu-latest=my-build-server:latest --secret-file my.secrets --artifact-server-path act-cli-temp-artifacts --var-file configurationVariables.secrets --pull=false # -j 'provision' 
