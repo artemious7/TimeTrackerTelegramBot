25444cdfa2388add574e39ad22f91fe51fd5c277
Artemious <16724889+artemious7@users.noreply.github.com>
2024-10-22 21:58:17 +0100

    set working-directory to infra for all steps

--- build-github-actions/act-cli.sh
+++ build-github-actions/act-cli.sh
@@ -1,9 +1,4 @@
 docker build -t my-build-server:latest ./build-github-actions
 
-act \
--W ".github/workflows/provision-deploy.yml" \
--s GITHUB_TOKEN="$(gh auth token)" \
--P ubuntu-latest=my-build-server:latest \
---secret-file my.secrets \
---artifact-server-path act-cli-temp-artifacts \
---var-file configurationVariables.secrets
+# Act CLI docs: https://nektosact.com/usage/index.html
+clear | act  -W ".github/workflows/provision-deploy.yml"  -s GITHUB_TOKEN="$(gh auth token)"  -P ubuntu-latest=my-build-server:latest  --secret-file my.secrets  --artifact-server-path act-cli-temp-artifacts  --var-file configurationVariables.secrets --pull=false # -j 'provision' 
